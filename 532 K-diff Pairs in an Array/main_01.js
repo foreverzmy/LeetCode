1f83e8d647806ff565ab469748e8161b2a07d2d7
MervynZ <foreverzmyer@gmail.com>
2018-02-04 21:44:01 +0800

    feat: #532

--- 532 K-diff Pairs in an Array/main_01.js
+++ 532 K-diff Pairs in an Array/main_01.js
@@ -3,10 +3,28 @@
  * @param {number} k
  * @return {number}
  */
-var findPairs = function(nums, k) {
+var findPairs = function (nums, k) {
   let dict = {};
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
     const tmp = nums[i];
+    if (dict[tmp] === undefined) {
+      dict[tmp] = 1;
+    } else {
+      dict[tmp]++;
+    }
   }
+
+  for (let key in dict) {
+    if (k === 0 & dict[key] > 1) {
+      count++;
+    }
+    if (k > 0 && dict[~~key + k]) {
+      count++;
+    }
+  }
+
+  return count;
 };
+
+console.log(findPairs([1, 1, 3, 3, 1], 2))
